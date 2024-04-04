e573c7fa0b3059f168017b8a6bc0909fa99c4821
DoQuocBinh <binhdq@fpt.edu.vn>
2024-04-04 16:11:29 +0700

    aa

---
+++ SIMS_IT0602/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SIMS_IT0602.Models
+{
+	public class Course
+	{
+		public int Id { get; set; }
+		public String Name { get; set; }
+		public int Lecturer { get; set; }
+		public Course()
+		{
+		}
+	}
+}
+
